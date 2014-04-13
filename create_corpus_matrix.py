f3f6add831299119a63da8cc1cd9c24cfbf9aaf1
Julian Katz-Samuels <jkatzsam@umich.edu>
2014-04-13 00:07:28 -0400

    made topic model for list of triples, added features to create_corpus_matrix for eliminating uncommon words

--- create_corpus_matrix.py
+++ create_corpus_matrix.py
@@ -25,14 +25,14 @@ class Corpus:
 		self.stop_words_file = stop_words_file
 		self.data = []
 		self.triples = []
-		self.filtered_data = [] #TODO QUESTION: is this adding too much to class? 
-		self.matrix = np.zeros((1,1))
-		self.col_doc_map = {} #map from columns of matrix to document ids
-		self.doc_col_map = {} #map from document ids to columns of matrix
-		self.row_word_map = {} #map from rows of matrix to words
-		self.word_row_map = {} #map from words to rows of matrix
-		self.vocab_size = 0 #vocab size of corpus
-		self.doc_size = 0 #number of documents in corpus
+		self.triples_id = []
+		self.id_doc_map = {} #map from columns of matrix to document ids
+		self.doc_id_map = {} #map from document ids to columns of matrix
+		self.id_word_map = {} #map from rows of matrix to words
+		self.word_id_map = {} #map from words to rows of matrix
+		self.num_types = 0 #vocab size of corpus
+		self.num_docs = 0 #number of documents in corpus
+		self.data_matrix = None
 		self.syn_types = ['nsubj', 'agent', 'dobj', 'nsubjpass', 
 						'iobj', 'prep_', 'appos', 'amod', 'nn']
 		self.words_not_wanted = ["john", 
@@ -62,41 +62,92 @@ class Corpus:
 				self.words_not_wanted.extend(row)
 
 
-	def create_triples(self):
+	def create_triples(self, threshold = 2):
 		"""
+		input:
+			threshold is the number of articles a word needs
+			to appear in for it to be included in the dataset
+
 		Only keep tuples of certain syntatic types; extract
 		word that is not Kerry from data
+
+		Note: consider adding some stuff here to filter out some data.
+		For example, if an article mentions Kerry once, maybe I should
+		just not include it. Or, if there are words that occur very frequently
+		or infrequently I should take them out. 
 		"""
 		#counters for number of unique words and documents
 		word_counter = collections.Counter()
 		doc_counter = collections.Counter()
+
+		#list for word doc pairs
+		word_doc_list = []
+		#dict for counting number of documents a word appears in
+		word_doc_dict = collections.defaultdict(list)
 		for elt in self.data:
 			tup, doc_id = ast.literal_eval(elt[3]), elt[7]
+
+			syntax_type = tup[0]
+			word_1 = tup[1].encode('ascii','ignore').lower()
+			word_2 = tup[2].encode('ascii','ignore').lower()
+
+			if word_1 not in self.words_not_wanted:
+				word_doc_list.append([word_1, doc_id, syntax_type])
+				word_doc_dict[word_1].append(doc_id)
+			if word_2 not in self.words_not_wanted:
+				word_doc_list.append([word_2, doc_id, syntax_type])
+				word_doc_dict[word_2].append(doc_id)
+
+		#add to unwanted list if a word appears in fewer than 5 documents
+		for word, doc in word_doc_dict.iteritems():
+			if len(doc) < threshold:
+				self.words_not_wanted.append(word)
+
+		print "True vocab size: " + str(len([i for i in map( len, word_doc_dict.values()) if i > 1]))
+
+		for word, doc_id, syn_rel in word_doc_list:
 			#grab tuples of specific syntatic types
-			if containment(self.syn_types, tup):
-				#grab the word that is not among words I don't want
-				#make into LOWERCASE string (from unicode)
-				#make the doc_id an int to sort the list later
-				word_1 = str(tup[1]).lower()
-				word_2 = str(tup[2]).lower()
-				if word_1 not in self.words_not_wanted:
-					self.triples.append([(word_1, doc_id, 0)])
-					word_counter[word_1] += 1
-					doc_counter[doc_id] += 1
-				if word_2 not in self.words_not_wanted:
-					self.triples.append([(word_2, doc_id, 0)])
-					word_counter[word_2] += 1
-					doc_counter[doc_id] += 1
+			if (containment(self.syn_types, syn_rel) 
+					and word not in self.words_not_wanted):
+				self.triples.append([word, doc_id, 0])
+				word_counter[word] += 1
+				doc_counter[doc_id] += 1
+
+		self.num_types = len(word_counter)
+		self.num_docs = len(doc_counter)
+
+		#create ids for data
 		self.vocab_size = len(word_counter)
 		self.doc_size = len(doc_counter)
 
+		#create map
+		words = word_counter.keys()
+		docs = doc_counter.keys()
+
+		incrementer = 0
+		for doc in docs:
+			self.id_doc_map[incrementer] = doc
+			self.doc_id_map[doc] = incrementer
+			incrementer += 1
+
+		incrementer = 0
+		for word in words:
+			self.id_word_map[incrementer] = word
+			self.word_id_map[word] = incrementer
+			incrementer += 1
+
+		for triple in self.triples:
+			triple_id = [self.word_id_map[triple[0]], self.doc_id_map[triple[1]]] + [0]
+			self.triples_id.append(triple_id)
 
 
+	def create_matrix(self):
+		self.matrix = np.zeros((self.vocab_size, self.doc_size))
+		for word, doc, topic in self.triples_id:
+			self.matrix[word, doc] += 1
 
-
-
-
-
+	def word_variance(self):
+		return np.var(self.matrix, 1)
 
 
 
@@ -110,4 +161,5 @@ if __name__ == "__main__":
 	test = Corpus(file_path, stop_words_path)
 	test.read_data()
 	test.create_triples()
+	test.create_matrix()
 
--- topic_model.py
+++ topic_model.py
@@ -34,11 +34,10 @@ beta = 0.1
 #modules
 import numpy as np
 import scipy as sp
-import csv
-import math
+import csv, math
 
 #custom_modules
-from create_corpus_matrix_v1 import Corpus
+from create_corpus_matrix import Corpus
 
 #helper functions
 def test_not_neg(a):
@@ -59,37 +58,39 @@ def save_data(data, outfile, header):
 #class
 class TopicModel:
 
-	def __init__(self, matrix, num_topics, beta):
+	def __init__(self, corpus, num_topics, beta):
 		self.alpha = float(50) / num_topics #scalar
 		self.beta = beta #scalar
-		self.num_words, self.num_docs = matrix.shape
+		self.num_types = corpus.num_types
+		self.num_docs = corpus.num_docs
+		self.id_doc_map = corpus.id_doc_map #map from columns of matrix to document ids
+		self.doc_id_map = corpus.doc_id_map #map from document ids to columns of matrix
+		self.id_word_map = corpus.id_word_map #map from rows of matrix to words
+		self.word_id_map = corpus.word_id_map #map from words to rows of matrix
 		self.num_topics = num_topics
-		self.data = matrix #words by documents matrix
+		self.data = corpus.triples_id #triples contain: token, document, topic assignment
 		self.doc_top_count = np.zeros((self.num_docs, self.num_topics)) #the number of words assigned to topic k in document d
-		self.word_top_count = np.zeros((self.num_words, self.num_topics)) #the number of times word w is assigned to topic k
+		self.word_top_count = np.zeros((self.num_types, self.num_topics)) #the number of times word w is assigned to topic k
 		self.top_count = np.zeros((1, self.num_topics)) #the total number of times any word is assigned to topic k
+		self.top_lag = np.zeros((1, self.num_topics)) #lag count for topic
 		self.prob = np.zeros((1,num_topics)) #probability of topics
-		self.doc_top_distr = np.zeros((self.num_docs, self.num_topics)) #document topic distributions
-		self.word_top_distr = np.zeros((self.num_words, self.num_topics)) #word number distributions
-		self.log_likelihood = 0 #log_likelihood
+		self.doc_top_distr = np.zeros((self.num_docs, self.num_topics)) #doc topic distributions: estimated using counts from Gibbs sampler
+		self.doc_top_lag = np.zeros((self.num_docs, self.num_topics)) #counts using lag and burn
+		self.word_top_distr = np.zeros((self.num_types, self.num_topics)) #word number distributions
+		self.word_top_lag = np.zeros((self.num_types, self.num_topics)) #word topic distributions: sampled from gibbs sampler using lag and burn
+		self.log_likelihood = None #log_likelihood
 
-		#Randomly initialize word to topic assignments
-		self.word_top_assign = np.random.random_integers(0, self.num_topics-1,(1, self.num_words)) #current assignment for each of the N words in the corpus
+		#randomly initialize word to topic assignments
+		for i in range(len(self.data)):
+			topic_assignment = np.random.random_integers(0, self.num_topics - 1)
+			self.data[i][2] = topic_assignment
 
-		#increment words_top_count
-		for word in range(self.num_words):
-			topic = self.word_top_assign[0, word]
-			self.word_top_count[word, topic] = sum(self.data[word, :])
-
-		#increment doc_top_count
-		for doc in range(self.num_docs):
-			for top in range(self.num_topics):
-				#make a matrix of 0 1s where 1 if it is the topic and 0 otherwise
-				topic_vec = -abs(np.sign(self.word_top_assign-top)) + 1
-				self.doc_top_count[doc, top] = np.dot(topic_vec, np.reshape(self.data[:, doc], (-1, 1)))
-
-		#increment top_count
-		self.top_count = np.matrix(np.sum(self.word_top_count, 0))
+		#increment words_top_count, top_cont, doc_top_count
+		for triple in self.data:
+			word, doc, topic = triple
+			self.word_top_count[word, topic] += 1 
+			self.doc_top_count[doc, topic] += 1
+			self.top_count[0, topic] += 1
 
 	def words_in_doc(self, doc):
 		"""
@@ -97,66 +98,79 @@ class TopicModel:
 		"""
 		return np.nonzero(self.data[:, doc])[0].tolist()
 
+
 	def docs_of_word(self, word):
 		"""
 		return docs that word appears in
 		"""
 		return np.nonzero(self.data[word, :])[0].tolist()
 
-	def gibbs(self, num_iterations):
+
+	def gibbs(self, num_iterations = 5000, burn = 750, lag = 50):
 		"""
 		input: 
-			num_iterations
+			num_iterations: number of iterations to perform the algorithm
+			burn: number of iterations to wail until collecting samples
+			lag: number of iterations to wait between samples to lessen autocorrelation
+
 
 		updates:
-			the gibbs sampler for a particular document
+			the gibbs sampler
 		"""
-		iter = 0
-		while iter < num_iterations:
-			iter += 1
-			print("iteration " + str(iter))
-			for doc in range(self.num_docs):
-				#only iterate through words in each document
-				for word in self.words_in_doc(doc):
-					topic_old = self.word_top_assign[0, word]
-					#decrement
-					#I shouldn't need this max thing, but can't figure out why I sometimes get -1. TODO: fix later. 
-					self.doc_top_count[doc, topic_old] -= max(self.doc_top_count[doc, topic_old] - 1, 0) 
-					self.word_top_count[word, topic_old] -= 1 
-					self.top_count[0, topic_old] -= 1
+		iteration = 0
+		lag_counter = 0
+		while iteration < num_iterations:
+			iteration += 1
+			print("iteration " + str(iteration))
+			for i in range(len(self.data)):
+				word, doc, topic_old = self.data[i]
+				#decrement
+				#I shouldn't need this max thing, but can't figure out why I sometimes get -1. TODO: fix later. 
+				self.doc_top_count[doc, topic_old] -= max(self.doc_top_count[doc, topic_old] - 1, 0) 
+				self.word_top_count[word, topic_old] -= 1 
+				self.top_count[0, topic_old] -= 1
 
-					# calculate topic probabilities
-					for k in range(self.num_topics):
-						self.prob[0, k] = (float(self.doc_top_count[doc, k] + self.alpha) /  \
-											(self.doc_top_count[doc, k] + \
-												self.num_topics * self.alpha)) * \
-										(float(self.word_top_count[word, k] + self.beta) / \
-										(self.top_count[0, k] + self.beta * self.num_words))
-					
-					#get rid of zero values
-					smoothed_prob = np.maximum(self.prob, np.zeros((1,self.num_topics)) + .000001)					
+				# calculate topic probabilities
+				for k in range(self.num_topics):
+					self.prob[0, k] = (float(self.doc_top_count[doc, k] + self.alpha) /  \
+										(self.doc_top_count[doc, k] + \
+											self.num_topics * self.alpha)) * \
+									(float(self.word_top_count[word, k] + self.beta) / \
+									(self.top_count[0, k] + self.beta * self.num_types))
+				
+				#get rid of zero values
+				smoothed_prob = np.maximum(self.prob, np.zeros((1,self.num_topics)) + .000001)					
 
-					#sample and assign a topic assignment for the word
-					normalized_prob = smoothed_prob[0,:-1] / smoothed_prob.sum()
-					topic_list = np.random.multinomial(1, normalized_prob.tolist(), 1) #maybe can vectorize the whole algorithm
-					topic_new = np.nonzero(topic_list)[1][0]
-					self.word_top_assign[0, word] = topic_new
+				#sample and assign a topic assignment for the word
+				normalized_prob = smoothed_prob[0,:-1] / smoothed_prob.sum()
+				topic_list = np.random.multinomial(1, normalized_prob.tolist(), 1) 
+				topic_new = np.nonzero(topic_list)[1][0]
+				self.data[i] = [word, doc, topic_new]
 
-					#increment
-					self.doc_top_count[doc, topic_new] += 1
-					self.word_top_count[word, topic_new] += 1
-					self.top_count[0, topic_new] += 1
+				if lag_counter >= lag:
+					self.word_top_lag[word, topic_new] += 1
+					self.doc_top_lag[doc, topic_new] += 1
+					self.top_lag[0, topic_new] += 1
+
+				#increment
+				self.doc_top_count[doc, topic_new] += 1
+				self.word_top_count[word, topic_new] += 1
+				self.top_count[0, topic_new] += 1
+			if iteration >= burn:
+				lag_counter += 1
+
 
 	def comp_word_top(self):
 		"""
 		compute the word_top_distr
 		"""
 		for topic in range(self.num_topics):
-			for word in range(self.num_words):
+			for word in range(self.num_types):
 				self.word_top_distr[word, topic] = \
-									float(self.word_top_count[word, topic] + self.beta) \
-										/ (self.top_count[0,topic] + self.beta * self.num_words)
+									float(self.word_top_lag[word, topic] + self.beta) \
+										/ (self.top_lag[0,topic] + self.beta * self.num_types)
 				print("word_topic distribution: " + str(self.word_top_distr[word, topic]))
+
 
 	def comp_doc_top(self):
 		"""
@@ -165,9 +179,10 @@ class TopicModel:
 		for topic in range(self.num_topics):
 			for doc in range(self.num_docs):
 				self.doc_top_distr[doc, topic] = \
-									float(self.doc_top_count[doc, topic] + self.alpha) \
-										/ (sum(self.doc_top_count[doc, :]) + self.num_topics * self.alpha)
+									float(self.doc_top_lag[doc, topic] + self.alpha) \
+										/ (sum(self.doc_top_lag[doc, :]) + self.num_topics * self.alpha)
 				print("document topic distribution: " + str(self.doc_top_distr[doc, topic]))
+
 
 	def comp_likelihood(self):
 		"""
@@ -177,23 +192,17 @@ class TopicModel:
 		if I need to do this at some point.
 		"""
 		log_likelihood = 0
-		for word in range(self.num_words):
+		for word in range(self.num_types):
 			word_prob = 0
-			docs = self.docs_of_word(word)
 			for topic in range(self.num_topics):
 				#probability of word given topic
 				word_topic_prob = self.word_top_distr[word, topic]
-				for doc in docs:
+				for doc in range(self.num_docs):
 					topic_prob = self.doc_top_distr[doc, topic]
 					word_prob += word_topic_prob * topic_prob
 			log_likelihood += math.log1p(word_prob)
 		self.log_likelihood = log_likelihood
 
-
-	def cross_validate(self):
-		"""
-		perform cross-validation
-		"""
 
 	def save_data(self, out_path):
 		doc_top_distr_list = self.doc_top_distr.tolist()
@@ -210,7 +219,7 @@ class TopicModel:
 
 
 ##Functions
-def select_model(topics_list, data_matrix):
+def select_model(topics_list, data_matrix, outpath, num_iterations=10000, burn = 1000, lag = 50):
 	"""
 	Input: 
 		list of topic numbers to test
@@ -223,10 +232,11 @@ def select_model(topics_list, data_matrix):
 	for topic_num in topics_list:
 		print("topic number: " + str(topic_num))
 		topic = TopicModel(data_matrix, topic_num, .5)
-		topic.gibbs(200)
+		topic.gibbs(num_iterations, burn, lag)
 		topic.comp_word_top()
 		topic.comp_doc_top()
 		topic.comp_likelihood()
+		topic.save_data(out_path + "topic_num_" + str(topic_num))
 		topic_likelihoods.append([topic_num, topic.log_likelihood])
 	return topic_likelihoods
 
@@ -240,56 +250,56 @@ if __name__ == "__main__":
 
 	test = Corpus(file_path, stop_words_path)
 	test.read_data()
-	test.filter_data()
-	test.create_matrix_data_map()
-	test.create_matrix()
+	test.create_triples()
 
 
-#object tests
-# topic = TopicModel(test.matrix, 3, .5)
-# topic.gibbs(100)
-# topic.comp_word_top()
-# topic.comp_doc_top()
-# topic.comp_likelihood()
-# print "The log-likelihood is " + str(topic.log_likelihood)
-# topic.save_data(out_path)
+	#object tests
+	# topic = TopicModel(test, 3, .5)
+	# topic.gibbs(50)
+	# topic.comp_word_top()
+	# topic.comp_doc_top()
+	# topic.comp_likelihood()
+	# print "The log-likelihood is " + str(topic.log_likelihood)
+	# print "Word topic variance: " + str(np.var(topic.word_top_distr, 0))
+	# print "Word topic mean: " + str(np.mean(topic.word_top_distr, 0))
+	# topic.save_data(out_path)
 
-#print out topic word distribution
-#and document topic distribution matrices
-# print "the document topic distribution matrix \n" + str(topic.doc_top_distr)
-# print "the word topic distribution matrix \n" + str(topic.word_top_distr)
-
-
-
-# #test count matrices
-# test_not_neg(topic.top_count)
-# test_not_neg(topic.doc_top_count) 
-# test_not_neg(topic.word_top_count)
+	#print out topic word distribution
+	#and document topic distribution matrices
+	# print "the document topic distribution matrix \n" + str(topic.doc_top_distr)
+	# print "the word topic distribution matrix \n" + str(topic.word_top_distr)
 
 
-# #Test Initialization
-# test_init = TopicModel(test.matrix, 5, .1)
 
-# test_not_neg(test_init.top_count)
-# test_not_neg(test_init.doc_top_count) 
-# test_not_neg(test_init.word_top_count)
-
-# x = np.zeros((test_init.num_docs, test_init.num_topics))
-# for doc in range(test_init.num_docs):
-# 	doc_word_list = test_init.words_in_doc(doc)
-# 	for word in doc_word_list:
-# 		topic = test_init.word_top_assign[0, word]
-# 		x[doc, topic] += 1
-# if np.sum(x - test_init.doc_top_count) == 0:
-# 	print("initialization is not a problem")
-# else:
-# 	print('initialization is a problem')
+	# #test count matrices
+	# test_not_neg(topic.top_count)
+	# test_not_neg(topic.doc_top_count) 
+	# test_not_neg(topic.word_top_count)
 
 
-# #model selection tests
-	topics_to_check = map(lambda x: x + 2 , range(15))
+	# #Test Initialization
+	# test_init = TopicModel(test.matrix, 5, .1)
+
+	# test_not_neg(test_init.top_count)
+	# test_not_neg(test_init.doc_top_count) 
+	# test_not_neg(test_init.word_top_count)
+
+	# x = np.zeros((test_init.num_docs, test_init.num_topics))
+	# for doc in range(test_init.num_docs):
+	# 	doc_word_list = test_init.words_in_doc(doc)
+	# 	for word in doc_word_list:
+	# 		topic = test_init.word_top_assign[0, word]
+	# 		x[doc, topic] += 1
+	# if np.sum(x - test_init.doc_top_count) == 0:
+	# 	print("initialization is not a problem")
+	# else:
+	# 	print('initialization is a problem')
+
+
+	# #model selection tests
+	topics_to_check = map(int, np.linspace(2,14, 5))
 	model_select_path = "/Users/jkatzsamuels/Desktop/Courses/Natural Language Processing/Project/Code/topic_model_data/model_selection_data_v1.csv"
-	model_select_data = select_model(topics_to_check, test.matrix)
+	model_select_data = select_model(topics_to_check, test, "/Users/jkatzsamuels/Desktop/Courses/Natural Language Processing/Project/Code/topic_model_data/")
 	save_data(model_select_data, model_select_path, ["number of topics", "log_likelihood"])
 
 
